f2b0d6cd83ea80b2ac32a76ead31fa0e171d2bea
jayanthnandagiri <nandagiri.jayanth@gmail.com>
2024-04-10 08:55:16 +0000

    Init commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.2.0"
+    }
+  }
+ backend "gcs" {
+    bucket  = "roidtc-240408-user501-tf-state"
+    prefix  = "terraform/state"
+  }
+
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
